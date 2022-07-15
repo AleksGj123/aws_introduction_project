ab7bf65ec0846673ddc8f84dcd2a5f1adb0c7fa6
Aleksandar Gjurcinov <aleksandar.gjurcinov@xl2.de>
2022-07-15 11:59:41 +0200

    updated function

--- process-data-fnc.js
+++ process-data-fnc.js
@@ -1,71 +1,81 @@
-const https = require('https');
-var AWS = require('aws-sdk');
-// Set the region 
-//AWS.config.update({region: 'REGION'});
-
-// Create the DynamoDB service object
+const https = require('https')
+const AWS = require('aws-sdk')
 
 function getRequest(url) {
   
   return new Promise((resolve, reject) => {
     https.get(url, res => {
-      let rawData = '';
+      let rawData = ''
 
       res.on('data', chunk => {
-        rawData += chunk;
-      });
+        rawData += chunk
+      })
 
       res.on('end', () => { 
-          resolve(rawData);
-        });
-    });
-  });
+          resolve(rawData)
+        })
+    })
+  })
 }
 
 async function writeIntoDB(location, temperature){
-  var ddb = new AWS.DynamoDB();
-  var timestamp = Date.now()
-  console.log(timestamp)
+  const ddb = new AWS.DynamoDB()
+  const timestamp = new Date()
+  const timestampIso = timestamp.toISOString()
+  console.log(timestampIso)
 
-  var params = {
+  const params = {
     TableName: 'weather-info',
     Item: {
-      'timestamp' : {S: timestamp},
+      'timestamp' : {S: timestampIso},
       'location' : {S: location},
       'temperature' : {N: temperature}
     }
-  };
-
-  console.log("test1")
+  }
 
   try {
     const data = await ddb.putItem(params).promise()
-    console.log("Success")
+    console.log('Success')
     console.log(data)
     return data
   } catch (err) {
-    console.log("Failure", err.message)
+    console.log('Failure', err.message)
     // there is no data here, you can return undefined or similar
   }
-
-  console.log("test2")
   
 }
 
+async function getParameter(parameterName){
+  const ssm = new AWS.SSM()
+  const data = await ssm
+        .getParameter({ Name: parameterName})
+        .promise()
+  const param = data.Parameter || {}
+  const value = param.Value || ''
+  return value
+}
+
 exports.handler = async event => {
+
+    const urlParameter = '/parameters/url'
   
-    const url = 'https://nuakw3sawarqcxlu5pvieox4by0gfvpk.lambda-url.eu-central-1.on.aws/';
-    const result = await getRequest(url)
+    // get URL:
+    const urlParamterValue = await getParameter(urlParameter)
+    console.log('Value is: ' + urlParamterValue)
+
+    // load wearther info
+    const result = await getRequest(urlParamterValue)
     const json = JSON.parse(result)
     console.log('json is: ', json)
     console.log('location is: ' + json.location.name)
     console.log('temperature is: ' + json.current.temp_c)
 
+    // write into database
     await writeIntoDB(json.location.name, json.current.temp_c.toString())
     
     return {
       statusCode: 200,
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(json),
-    };
-};
+    }
+}
